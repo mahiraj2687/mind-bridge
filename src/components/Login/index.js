a7dcc3136be3712f5c1feac856afac99bcc5bcbc
Rajesh <pylarajesh0@gmail.com>
2023-03-02 22:31:23 +0530

    Main

---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+import {Component} from "react"
+import "./index.css"
+
+class Login extends Component{
+    render(){
+        console.log("loaded")
+        return (
+            <div className="loginFormContainer">
+        <h1 className="heading">Rajesh</h1>
+    </div>
+        )
+    }
+}
+
+export default Login
