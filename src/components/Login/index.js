bb3da062ed411c57e20b348d4696f1c67b0994f2
Rajesh <pylarajesh0@gmail.com>
2024-09-11 00:32:01 +0530

    just some code added for debug

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,6 +4,7 @@ import "./index.css"
 class Login extends Component{
     render(){
         console.log("loaded")
+        console.log('new changes made just now')
         return (
             <div className="loginFormContainer">
         <h1 className="heading">Rajesh</h1>
