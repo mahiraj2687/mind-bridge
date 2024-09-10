e151eed5e9fcd6c75983443f9ad61fb65a563f72
Rajesh <pylarajesh0@gmail.com>
2024-09-11 00:39:12 +0530

    checking

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,6 +6,7 @@ class Login extends Component{
         console.log("loaded")
         console.log('new changes made just now')
         console.log('another request')
+        console.log('all')
         return (
             <div className="loginFormContainer">
         <h1 className="heading">Rajesh</h1>
